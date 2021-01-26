540cb579953f59a905cceae47103f8dea8e32cfe
xdboyx <65160967+xdboyx@users.noreply.github.com>
2021-01-26 15:42:22 +0800

    css demo init

---
+++ js/all.js
@@ -0,0 +1,9 @@
+export default {
+    text1(){
+        console.dir('test1111')
+    },
+    test2(){
+        console.dir('test2222')
+    }
+
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import script from './all.js'
+
+script.text1()
+script.test2()
